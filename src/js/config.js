006821f2b5f0bf64808c3909747fb6ccde740e8a
dukeviet <dukeviet@hotmail.com>
2021-12-09 17:54:01 +0700

    init

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9c2f3210-695a-49b6-b36e-da039790b627';
+export const MODAL_CLOSE_SEC = 2.5;
+//API KEY 9c2f3210-695a-49b6-b36e-da039790b627
